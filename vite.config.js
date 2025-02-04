2b390a130ee6ba9f1dd3a94c94ff40b5899b519a
Andrey Drapatiy <drapatiy92@gmail.com>
2025-02-04 14:29:16 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
+    lib: {
+      entry: 'src/widget.js',  // <--- Ensure correct entry point
+      name: 'Widget',
+      fileName: 'widget',
+      formats: ['umd'],  // Use UMD format for browser compatibility
+    },
+    outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: "index.html",
-        widget: "src/widget.js",
+      output: {
+        entryFileNames: 'assets/widget.js',
       },
     },
   },
